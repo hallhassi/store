40af6817e6aa837a4618043460d2f60b0b01ef15
hallhassi <hallhassi@gmail.com>
2026-03-20 19:00:46 -0500

    Update create-checkout.js

--- netlify/functions/create-checkout.js
+++ netlify/functions/create-checkout.js
@@ -1,3 +1,4 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 exports.handler = async (event) => {
     try {
         const { cart, success_url } = JSON.parse(event.body); // <-- must be in here
